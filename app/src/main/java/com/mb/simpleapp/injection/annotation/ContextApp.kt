a6900823355cb11899e451fd191b95eadc5fdac9
Michal Bujalski <michal@tsh.io>
2017-07-24 14:34:05 +0200

    Added core dagger infrastructure.

---
+++ app/src/main/java/com/mb/simpleapp/injection/annotation/ContextApp.kt
@@ -0,0 +1,6 @@
+package com.mb.simpleapp.injection.annotation
+
+import javax.inject.Qualifier
+import javax.inject.Scope
+
+@Qualifier annotation class ContextApp
---
+++ app/src/main/java/com/mb/simpleapp/injection/annotation/PerActivity.kt
@@ -0,0 +1,5 @@
+package com.mb.simpleapp.injection.annotation
+
+import javax.inject.Scope
+
+@Scope annotation class PerActivity
