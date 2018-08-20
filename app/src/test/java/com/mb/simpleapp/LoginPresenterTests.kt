f27a8eeed37b08bb1fa070549b9b1ba15840cfcd
Michal Bujalski <michal@tsh.io>
2018-08-20 08:11:50 +0200

    Unit tests update

--- app/src/test/java/com/mb/simpleapp/LoginPresenterTests.kt
+++ app/src/test/java/com/mb/simpleapp/LoginPresenterTests.kt
@@ -5,30 +5,31 @@ import com.mb.simpleapp.features.login.LoginPresenter
 import com.mb.simpleapp.features.login.LoginPresenterImpl
 import com.mb.simpleapp.features.login.LoginView
 import com.nhaarman.mockito_kotlin.whenever
-import io.reactivex.Completable
-import io.reactivex.android.plugins.RxAndroidPlugins
-import io.reactivex.schedulers.Schedulers
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito
-import org.mockito.Mockito.*
+import org.mockito.Mock
+import org.mockito.Mockito.anyString
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+import tea.Program
 
 class LoginPresenterTests {
-    private lateinit var userInteractor: UserInteractor
-    private lateinit var presenter:LoginPresenter
-    private lateinit var view:LoginView
+    @Mock lateinit var userInteractor: UserInteractor
+    @Mock lateinit var presenter:LoginPresenter
+    @Mock lateinit var view:LoginView
 
     @JvmField @Rule val rule:RxSchedulerRule = RxSchedulerRule()
 
     @Before fun setUp(){
-        userInteractor = mock(UserInteractor::class.java)
-        view = mock(LoginView::class.java)
-        presenter = LoginPresenterImpl(userInteractor,view)
+        MockitoAnnotations.initMocks(this)
+        presenter = LoginPresenterImpl(userInteractor,view, Program(AndroidSchedulers.mainThread()))
     }
 
     @Test fun test_successfulLogin(){
-        whenever(userInteractor.login(anyString(),anyString())).thenReturn(Completable.complete())
+        whenever(userInteractor.login(anyString(),anyString())).thenReturn(Single.just(true))
         presenter.initLogin("tom@test.com","someLongPass")
         verify(view).showProgress()
         verify(view).hideProgress()
@@ -52,7 +53,7 @@ class LoginPresenterTests {
     @Test fun test_errorHandling(){
         val errMsg = "Improper argument"
         whenever(userInteractor.login(anyString(),anyString()))
-                .thenReturn(Completable.error(IllegalArgumentException(errMsg)))
+                .thenReturn(Single.error(IllegalArgumentException(errMsg)))
         presenter.initLogin("tom@example.com","someLongPass")
         verify(view).onLoginError(errMsg)
     }
